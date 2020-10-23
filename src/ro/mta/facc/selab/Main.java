e74193990d043e2d180ec01481090e20201d6f47
basicjunior <stanmihailalexandru@gmail.com>
2020-10-23 20:00:45 +0300

    Second Commit
    Adaugare fisiere Proiect Biblioteca
    
    Signed-off-by: basicjunior <stanmihailalexandru@gmail.com>

---
+++ src/ro/mta/facc/selab/Main.java
@@ -0,0 +1,34 @@
+package ro.mta.facc.selab;
+
+import java.util.concurrent.RunnableScheduledFuture;
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Carte c = new Carte("Biblia Pierduta", "Igor Bergler", 2017, 600);
+        Biblioteca b = new Biblioteca();
+        b.add(c);
+
+        b.add(new Carte("Invitatie la vals", "Mihail Dumes", 1910, 250));
+        b.add(new Carte("Invitatie la vals", "Mihail Dumes", 1910, 250));
+
+        b.sort();
+        System.out.println(b);
+
+        MyThread thread = new MyThread();
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("alt thread!");
+            }
+        }).start();
+    }
+}
